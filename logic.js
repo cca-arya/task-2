7ea03228efec11c921d70c9e75599f8522bfa81f
Arya-Shidore <aryashidore2002@gmail.com>
2024-06-05 13:27:50 +0530

    added a few changes

---
+++ logic.js
@@ -0,0 +1,12 @@
+
+// const increment = document.getElementById('increment');
+//         const decrement = document.getElementById('decrement');
+//         const count = document.getElementById('count');
+
+//         increment.addEventListener('click', () => {
+//             count.innerHTML = parseInt(count.innerHTML) + 1;
+//         });
+
+//         decrement.addEventListener('click', () => {
+//             count.innerHTML = parseInt(count.innerHTML) - 1;
+//         });
